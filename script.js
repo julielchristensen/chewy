0d0f10ac9d8ec80b4e985223ad0f864e96013805
[julielchristensen] <[juli15f3@stud.kea.dk]>
2021-10-13 09:53:08 +0200

    rettelser

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const artists = gsap.utils.toArray(".artist");
+
+artists.forEach((element, index) => {
+  let items = element.querySelectorAll(":scope > *");
+  let distance = index % 2 == 0 ? -100 : 100;
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: element,
+      start: "top 50%",
+      toggleActions: "restart none none reverse",
+      //   markers: true,
+    },
+  });
+
+  tl.from(items, {
+    duration: 0.5,
+    xPercent: gsap.utils.wrap([-distance, distance]),
+  });
+  tl.from(items, { opacity: 0, duration: 0.3 }, 0);
+});
